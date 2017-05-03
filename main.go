d3288afdc922a98b7ae4266ee66ca2ee3b0a5b85
Minho <longfei6671@163.com>
2017-05-03 15:05:01 +0800

    添加配置说明

--- main.go
+++ main.go
@@ -3,6 +3,9 @@ package main
 import (
 	_ "github.com/go-sql-driver/mysql"
 	_ "github.com/lifei6671/godoc/routers"
+	_ "github.com/astaxie/beego/session/redis"
+	_ "github.com/astaxie/beego/session/memcache"
+	_ "github.com/astaxie/beego/session/mysql"
 	"github.com/astaxie/beego"
 	"github.com/lifei6671/godoc/commands"
 	"fmt"
