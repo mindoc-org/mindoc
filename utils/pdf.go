a438b8104d3640235186ffb3a43ecc01b1a145cb
Minho <longfei6671@163.com>
2017-05-05 18:16:05 +0800

    *

--- utils/pdf.go
+++ utils/pdf.go
@@ -1,28 +1,22 @@
 package utils
 
 import (
-	"github.com/signintech/gopdf"
+	"github.com/jung-kurt/gofpdf"
 	"github.com/astaxie/beego"
 )
 func ConverterPdf(output string,htmlList map[string]string) error {
-	pdf := gopdf.GoPdf{}
-	pdf.Start(gopdf.Config{PageSize: gopdf.Rect{W: 595.28, H: 841.89}})
+
+	pdf := gofpdf.New("P", "mm", "A4", "./static/pdf-fonts/msyh.ttf")
 
 	pdf.AddPage()
 
-	err := pdf.AddTTFFont("HDZB_5", "./static/pdf-fonts/msyh.ttf")
-	if err != nil {
-		beego.Error("ConverterPdf => ",err)
-		return err
-	}
-	err = pdf.SetFont("HDZB_5", "", 14)
-	if err != nil {
-		beego.Error("ConverterPdf => " , err)
-		return err
-	}
-	
-	pdf.Cell(nil, "您好")
-	pdf.WritePdf(output)
+	pdf.SetFont("微软雅黑","B",14)
+	pdf.Cell(40, 10, "Hello, world")
+	err := pdf.OutputFileAndClose("hello.pdf")
 
+	if err != nil {
+		beego.Error(err)
+		return err
+	}
 	return nil
 }
