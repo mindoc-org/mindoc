3091e93e3e17e9497ff64632e5fdc413327f4f14
Minho <longfei6671@163.com>
2017-05-11 17:15:56 +0800

    *

--- controllers/document.go
+++ controllers/document.go
@@ -645,7 +645,7 @@ func RecursiveFun(parent_id int,prefix,dpath  string,c *DocumentController,book 
 			paths.PushBack(fpath)
 
 
-			f, err := os.OpenFile(fpath, os.O_CREATE, 0777)
+			f, err := os.OpenFile(fpath, os.O_CREATE|os.O_RDWR, 0777)
 
 			if err != nil {
 				beego.Error(err)
