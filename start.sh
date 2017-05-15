fc6ec31b319de35c41d792671bb4774b20adbf45
Minho <longfei6671@163.com>
2017-05-15 16:19:59 +0800

    dockerfile添加CDN配置参数

--- start.sh
+++ start.sh
@@ -27,8 +27,20 @@ if [ ! -z $MYSQL_PASSWORD ] ; then
     sed -i 's/^db_password.*/db_password='$MYSQL_PASSWORD'/g' conf/app.conf
 fi
 
-if [ ! -z $HTTP_PORT] ; then
+if [ ! -z $HTTP_PORT ] ; then
     sed -i 's/^httpport.*/httpport='$HTTP_PORT'/g' conf/app.conf
+fi
+
+if [ ! -z $CDNJS ]; then
+    sed -i 's/^cdnjs.*/cdnjs='$CDNJS'/g' conf/app.conf
+fi
+
+if [ ! -z $CDNCSS ]; then
+    sed -i 's/^cdncss.*/cdncss='$CDNCSS'/g' conf/app.conf
+fi
+
+if [ ! -z $CDN ]; then
+    sed -i 's/^cdn.*/cdnjs='$CDN'/g' conf/app.conf
 fi
 
 sed -i 's/^runmode.*/runmode=prod/g' conf/app.conf
