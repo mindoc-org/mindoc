1b056796f6e8ebff7731fc897ed768c85707a136
Wolcen <wolcen@msn.com>
2017-07-04 19:31:24 +0800

    容器发布脚本修改

--- myrelease/build.sh
+++ myrelease/build.sh
@@ -1,17 +1,31 @@
 #!/bin/bash
 
-# clean
-rm -rf centos6-mindoc-source.tar.gz
-rm -rf centos6-mindoc/files/mindoc.tar.gz
-
+#在开发编译容器里
 cd ..
+git pull
+if [ -f "mindoc" ];then
+  rm -rf mindoc
+fi
+go build
+if [ ! -f "mindoc" ];then
+  echo "编译生成mindoc失败"
+  exit
+fi
+if [ -f "myrelease/centos6-mindoc/files/mindoc.tar.gz" ];then
+   rm -rf myrelease/centos6-mindoc/files/mindoc.tar.gz
+fi
 tar -zcf centos6-mindoc/files/mindoc.tar.gz . --exclude myrelease --exclude .git --exclude .github --exclude logs --exclude  database --exclude conf/app.conf --exclude *.go --exclude *.tmp --exclude appveyor.yml --exclude glide.yaml --exclude Dockerfile  --exclude start.h 
+
 cd myrelease
+if [ -f "centos6-mindoc-release.tar.gz" ];then
+  rm -rf centos6-mindoc-release.tar.gz
+fi
+tar -zcf centos6-mindoc-release.tar.gz centos6-mindoc
 
-tar -zcf centos6-mindoc-source.tar.gz centos6-mindoc
-
-#copy centos6-mindoc-source.tar.gz to docker manger and tar
-#cd centos6-mindoc-source
+# 以下在容器管理机上执行
+# 先复制 centos6-mindoc-release.tar.gz 到管理机
+#tar -zxf centos6-mindoc-release.tar.gz .
+#cd centos6-mindoc-release
 #docker build -t mindoc:1.0.0 .
 
 
--- myrelease/centos6-mindoc/files/inst.sh
+++ myrelease/centos6-mindoc/files/inst.sh
@@ -5,15 +5,4 @@ mkdir -p /usr/local/mindoc
 tar -zxf mindoc.tar.gz -C /usr/local/mindoc/
 mv app.conf /usr/local/mindoc/conf/app.conf
 mv start.sh /usr/local/mindoc/start.sh
-
-#install wkhtmlto
-yum install -y libXrender libXext fontconfig mkfontscale xz*
-xz -d wkhtmltox-0.12.4_linux-generic-amd64.tar.xz
-rm -rf wkhtmltox-0.12.4_linux-generic-amd64.tar.xz
-tar -xf wkhtmltox-0.12.4_linux-generic-amd64.tar -C /usr/local/
-mv msyh.ttc /usr/share/fonts/msyh.ttc
-mv simsun.ttc /usr/share/fonts/simsun.ttc
-cd /usr/share/fonts
-mkfontscale
-mkfontdir
-fc-cache
+chmod 777 /usr/local/mindoc/start.sh
