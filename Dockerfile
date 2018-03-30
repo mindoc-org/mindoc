79901b54674e80f4a8a8d6dbfab8dfca94a21b7c
trydofor <trydofor@gmail.com>
2018-03-30 22:44:01 +0800

    最小化docker 16M

--- Dockerfile
+++ Dockerfile
@@ -1,21 +1,41 @@
-FROM golang:1.10-alpine3.7
+FROM golang:1.10-alpine3.7 as builder
 
-# add china aliyun repo  新增了 alpine 3.6 的阿里源
-RUN cp   /etc/apk/repositories /etc/apk/repositories.back && \
-    echo "https://mirrors.aliyun.com/alpine/v3.6/main/" >  /etc/apk/repositories && \
-    echo "https://mirrors.aliyun.com/alpine/v3.6/community/" >> /etc/apk/repositories
+## add china aliyun repo  新增了 alpine 3.6 的阿里源
+#RUN cp   /etc/apk/repositories /etc/apk/repositories.back && \
+#    echo "https://mirrors.aliyun.com/alpine/v3.6/main/" >  /etc/apk/repositories && \
+#    echo "https://mirrors.aliyun.com/alpine/v3.6/community/" >> /etc/apk/repositories
 
 RUN apk add --update bash git make gcc g++
 
-ADD . /go/src/github.com/lifei6671/mindoc
-
-WORKDIR /go/src/github.com/lifei6671/mindoc
-
-RUN chmod +x start.sh
+ARG MINDOC_BUILD=/go/src/github.com/lifei6671/mindoc
+ADD . $MINDOC_BUILD
+WORKDIR $MINDOC_BUILD
 
 RUN   go get -u github.com/golang/dep/cmd/dep && dep ensure && \
-    CGO_ENABLE=1 go build -v -o mindoc_linux_amd64 -ldflags="-w -X main.VERSION=$TAG -X 'main.BUILD_TIME=`date`' -X 'main.GO_VERSION=`go version`'" && \
-        rm -rf commands controllers models modules routers tasks vendor docs search data utils graphics .git Godeps uploads/* .gitignore .travis.yml Dockerfile gide.yaml LICENSE main.go README.md conf/enumerate.go conf/mail.go install.lock
+      CGO_ENABLE=0 go build -v -o mindoc_linux_amd64 -ldflags="-w -X main.VERSION=$TAG -X 'main.BUILD_TIME=`date`' -X 'main.GO_VERSION=`go version`'" && \
+      rm -rf commands controllers models modules routers tasks vendor docs search data utils graphics .git Godeps uploads/* .gitignore .travis.yml Dockerfile gide.yaml LICENSE main.go README.md conf/enumerate.go conf/mail.go install.lock
 
+################
+# docker build -t trydofor/mindoc:0.10 .
+################
 
-CMD ["./start.sh"]
+FROM alpine:3.7
+
+LABEL maintainer="trydofor"
+
+ENV MINDOC_ROOT=/app
+ENV HTTP_PORT=8181
+ENV ZONEINFO=$MINDOC_ROOT/lib/time/zoneinfo.zip
+
+WORKDIR  $MINDOC_ROOT
+COPY --from=builder /go/src/github.com/lifei6671/mindoc .
+
+RUN chmod +x start.sh
+RUN mv conf conf.bak
+
+VOLUME $MINDOC_ROOT/conf
+VOLUME $MINDOC_ROOT/uploads
+EXPOSE $HTTP_PORT
+
+ENTRYPOINT ["./start.sh"]
+CMD [""]
--- start.sh
+++ start.sh
@@ -1,86 +1,87 @@
 #!/bin/sh
 set -e
 
-cd /go/src/github.com/lifei6671/mindoc/
+cd $MINDOC_ROOT
+appconf=conf/app.conf
 
-if [ ! -f "/go/src/github.com/lifei6671/mindoc/conf/app.conf" ] ; then
-    cp /go/src/github.com/lifei6671/mindoc/conf/app.conf.example /go/src/github.com/lifei6671/mindoc/conf/app.conf
-	sed -i "s#^db_adapter=.*#db_adapter=sqlite3#g" conf/app.conf
-	sed -i "s#^db_database.*#db_database=./database/mindoc.db#g" conf/app.conf
+if [ ! -f "$appconf" ] ; then
+    cp conf.bak/app.conf.example $appconf
+    sed -i "s#^db_adapter=.*#db_adapter=sqlite3#g" $appconf
+    sed -i "s#^db_database.*#db_database=./database/mindoc.db#g" $appconf
 fi
 
-if [ ! -z $DB_ADAPTER ]; then
-	sed -i "s#^db_adapter=.*#db_adapter=${DB_ADAPTER}#g" conf/app.conf
+if [ ! -z $DB_TYPE ]; then
+    sed -i "s#^db_adapter=.*#db_adapter=${DB_TYPE}#g" $appconf
 fi
 
-if [ ! -z $MYSQL_PORT_3306_TCP_ADDR ] ; then
-    sed -i 's/^db_host.*/db_host='$MYSQL_PORT_3306_TCP_ADDR'/g' conf/app.conf
+if [ ! -z $DB_HOST ] ; then
+    sed -i 's/^db_host.*/db_host='$DB_HOST'/g' $appconf
 fi
 
-if [ ! -z $MYSQL_PORT_3306_TCP_PORT ] ; then
-    sed -i 's/^db_port.*/db_port='$MYSQL_PORT_3306_TCP_PORT'/g' conf/app.conf
+if [ ! -z $DB_PORT ] ; then
+    sed -i 's/^db_port.*/db_port='$DB_PORT'/g' $appconf
 fi
 
-if [ ! -z $MYSQL_INSTANCE_NAME ] ; then
-    sed -i "s#^db_database.*#db_database=${MYSQL_INSTANCE_NAME}#g" conf/app.conf
+if [ ! -z $DB_NAME ] ; then
+    sed -i "s#^db_database.*#db_database=${DB_NAME}#g" $appconf
 fi
 
-if [ ! -z $MYSQL_USERNAME ] ; then
-    sed -i 's/^db_username.*/db_username='$MYSQL_USERNAME'/g' conf/app.conf
+if [ ! -z $DB_USER ] ; then
+    sed -i 's/^db_username.*/db_username='$DB_USER'/g' $appconf
 fi
 
-if [ ! -z $MYSQL_PASSWORD ] ; then
-    sed -i 's/^db_password.*/db_password='$MYSQL_PASSWORD'/g' conf/app.conf
+if [ ! -z $DB_PASS ] ; then
+    sed -i 's/^db_password.*/db_password='$DB_PASS'/g' $appconf
 fi
 
 if [ ! -z $HTTP_PORT ] ; then
-    sed -i 's/^httpport.*/httpport='$HTTP_PORT'/g' conf/app.conf
+    sed -i 's/^httpport.*/httpport='$HTTP_PORT'/g' $appconf
 fi
 
 if [ ! -z $CDNJS ]; then
-    sed -i "s#^cdnjs=.*#cdnjs=$CDNJS#g" conf/app.conf
+    sed -i "s#^cdnjs=.*#cdnjs=$CDNJS#g" $appconf
 fi
 if [ ! -z $CDNIMG ]; then
-    sed -i "s#^cdnimg=.*#cdnimg=$CDNIMG#g" conf/app.conf
+    sed -i "s#^cdnimg=.*#cdnimg=$CDNIMG#g" $appconf
 fi
 
 if [ ! -z $CDNCSS ]; then
-    sed -i "s#^cdncss=.*#cdncss=$CDNCSS#g" conf/app.conf
+    sed -i "s#^cdncss=.*#cdncss=$CDNCSS#g" $appconf
 fi
 
 if [ ! -z $CDN ]; then
-    sed -i "s#^cdn=.*#cdn=$CDN#g" conf/app.conf
+    sed -i "s#^cdn=.*#cdn=$CDN#g" $appconf
 fi
 
 if [ ! -z $CACHE ]; then
-    sed -i "s#cache=.*#cache=$CACHE#g" conf/app.conf
+    sed -i "s#cache=.*#cache=$CACHE#g" $appconf
 fi
 
 if [ ! -z $CACHE_PROVIDER ]; then
-    sed -i "s#cache_provider=.*#cache_provider=$CACHE_PROVIDER#g" conf/app.conf
+    sed -i "s#cache_provider=.*#cache_provider=$CACHE_PROVIDER#g" $appconf
 fi
 
 if [ ! -z $CACHE_MEMCACHE_HOST ]; then
-    sed -i "s#cache_memcache_host=.*#cache_memcache_host=$CACHE_MEMCACHE_HOST#g" conf/app.conf
+    sed -i "s#cache_memcache_host=.*#cache_memcache_host=$CACHE_MEMCACHE_HOST#g" $appconf
 fi
 
 if [ ! -z $CACHE_REDIS_HOST ]; then
-    sed -i "s#cache_redis_host=.*#cache_redis_host=$CACHE_REDIS_HOST#g" conf/app.conf
+    sed -i "s#cache_redis_host=.*#cache_redis_host=$CACHE_REDIS_HOST#g" $appconf
 fi
 
 if [ ! -z $CACHE_REDIS_DB ]; then
-    sed -i "s#cache_redis_db=.*#cache_redis_db=$CACHE_REDIS_DB#g" conf/app.conf
+    sed -i "s#cache_redis_db=.*#cache_redis_db=$CACHE_REDIS_DB#g" $appconf
 fi
 
 if [ ! -z $CACHE_REDIS_PASSWROD ]; then
-    sed -i "s#cache_redis_password=.*#cache_redis_password=$CACHE_REDIS_PASSWROD#g" conf/app.conf
+    sed -i "s#cache_redis_password=.*#cache_redis_password=$CACHE_REDIS_PASSWROD#g" $appconf
 fi
 
 if [ ! -z $BASEURL ]; then
-    sed -i "s#baseurl=.*#baseurl=$BASEURL#g" conf/app.conf
+    sed -i "s#baseurl=.*#baseurl=$BASEURL#g" $appconf
 fi
 
-sed -i 's/^runmode.*/runmode=prod/g' conf/app.conf
+sed -i 's/^runmode.*/runmode=prod/g' $appconf
 
-/go/src/github.com/lifei6671/mindoc/mindoc_linux_amd64 install
-/go/src/github.com/lifei6671/mindoc/mindoc_linux_amd64
+./mindoc_linux_amd64 install
+./mindoc_linux_amd64
