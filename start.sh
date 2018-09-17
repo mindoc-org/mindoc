b8c297015e2cadb8ef4c31994c1c24e9e3c308d6
lifei6671 <longfei6671@163.com>
2018-09-17 18:22:41 +0800

    fix:修复Dockerfile文件

--- start.sh
+++ start.sh
@@ -7,6 +7,6 @@ if [ ! -f "/mindoc/conf/app.conf" ] ; then
     cp /mindoc/conf/app.conf.example /mindoc/conf/app.conf
 fi
 
-
+export ZONEINFO=/mindoc/lib/time/zoneinfo.zip
 /mindoc/mindoc_linux_amd64 install
 /mindoc/mindoc_linux_amd64
