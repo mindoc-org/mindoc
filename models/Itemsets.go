2460a32ac5abb25a45f083ffdcbc230370af9fdd
lifei6671 <longfei6671@163.com>
2018-11-27 10:04:37 +0800

    fix:修复mysql兼容问题

--- models/Itemsets.go
+++ models/Itemsets.go
@@ -15,7 +15,7 @@ import (
 type Itemsets struct {
 	ItemId      int       `orm:"column(item_id);pk;auto;unique" json:"item_id"`
 	ItemName    string    `orm:"column(item_name);size(500)" json:"item_name"`
-	ItemKey     string    `orm:"column(item_key);size(200);unique" json:"item_key"`
+	ItemKey     string    `orm:"column(item_key);size(100);unique" json:"item_key"`
 	Description string    `orm:"column(description);type(text);null" json:"description"`
 	MemberId    int       `orm:"column(member_id);size(100)" json:"member_id"`
 	CreateTime  time.Time `orm:"column(create_time);type(datetime);auto_now_add" json:"create_time"`
