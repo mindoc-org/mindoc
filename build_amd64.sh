b00536f927daaa62e90632faa3b74ea619cd8efe
gsw945 <gsw945@foxmail.com>
2022-07-20 15:04:01 +0800

    移动favicon.ico到static目录

--- build_amd64.sh
+++ build_amd64.sh
@@ -16,5 +16,5 @@ cp -r * ../mindoc_linux_amd64
 cd ../mindoc_linux_amd64
 rm -rf cache commands controllers converter .git .github graphics mail models routers utils runtime conf/*.go
 rm appveyor.yml docker-compose.yml Dockerfile .travis.yml .gitattributes .gitignore go.mod go.sum main.go README.md simsun.ttc start.sh sync_host.sh build_amd64.sh build_musl_amd64.sh
-zip -r mindoc_linux_amd64.zip conf static uploads views lib mindoc_linux_amd64 favicon.ico LICENSE.md
+zip -r mindoc_linux_amd64.zip conf static uploads views lib mindoc_linux_amd64 LICENSE.md
 mv ./mindoc_linux_amd64.zip ../
--- views/document/history.tpl
+++ views/document/history.tpl
@@ -3,7 +3,7 @@
 <html lang="zh-cn">
 <head>
     <meta charset="utf-8">
-    <link rel="shortcut icon" href="{{cdnimg "/favicon.ico"}}">
+    <link rel="shortcut icon" href="{{cdnimg "/static/favicon.ico"}}">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
     <meta name="renderer" content="webkit" />
     <meta name="viewport" content="width=device-width, initial-scale=1">
