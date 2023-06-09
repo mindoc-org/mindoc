da555036709499eca3f726261f552d7b94154acd
Go-Go-Farther <sxp.wushu@gmail.com>
2023-06-09 11:17:07 +0800

    bugfix：1. 修复加密文章访问，非作者和管理员不能访问的问题。还有密码修改文案。 resolves mindoc-org/mindoc#849

--- controllers/BlogController.go
+++ controllers/BlogController.go
@@ -56,23 +56,14 @@ func (c *BlogController) Index() {
 		if blog.BlogStatus == "password" && password != blog.Password {
 			c.JsonResult(6001, i18n.Tr(c.Lang, "message.blog_pwd_incorrect"))
 		} else if blog.BlogStatus == "password" && password == blog.Password {
-			// If the password is correct, then determine whether the user is correct
-			if c.Member != nil && (blog.MemberId == c.Member.MemberId || c.Member.IsAdministrator()) {
-				/* Private blog is accessible only to author and administrator.
-				   Anonymous users are not allowed access. */
-				// Store the session value
-				_ = c.CruSession.Set(context.TODO(), blogReadSession, blogId)
-				c.JsonResult(0, "OK")
-			} else {
-				c.JsonResult(6002, i18n.Tr(c.Lang, "blog.private_blog_tips"))
-			}
+			// Store the session value for the next GET request.
+			_ = c.CruSession.Set(context.TODO(), blogReadSession, blogId)
+			c.JsonResult(0, "OK")
 		} else {
 			c.JsonResult(0, "OK")
 		}
-	} else if blog.BlogStatus == "password" &&
-		(c.CruSession.Get(context.TODO(), blogReadSession) == nil || // Read session doesn't exist
-			c.Member == nil || // Anonymous, Not Allow
-			(blog.MemberId != c.Member.MemberId && !c.Member.IsAdministrator())) { // User isn't author or administrator
+	} else if blog.BlogStatus == "password" && c.CruSession.Get(context.TODO(), blogReadSession) == nil && // Read session doesn't exist
+		(c.Member == nil || (blog.MemberId != c.Member.MemberId && !c.Member.IsAdministrator())) { // User isn't author or administrator
 		//如果不存在已输入密码的标记
 		c.TplName = "blog/index_password.tpl"
 	}
