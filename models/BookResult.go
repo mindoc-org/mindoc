81a3a5b32d482aae7401724a3daf335e58252829
FunCoder <qzzdy@foxmail.com>
2024-12-17 14:30:33 +0800

    发布pdf时候的默认发布者（项目填写了公司名称以公司名称为准）

--- models/BookResult.go
+++ models/BookResult.go
@@ -373,8 +373,12 @@ func (m *BookResult) Converter(sessionId string) (ConvertBookResult, error) {
 		Toc:          tocList,
 		More:         []string{},
 	}
+
 	if m.Publisher != "" {
 		ebookConfig.Footer = "<p style='color:#8E8E8E;font-size:12px;'>本文档由 <span style='text-decoration:none;color:#1abc9c;font-weight:bold;'>" + m.Publisher + "</span> 生成<span style='float:right'>- _PAGENUM_ -</span></p>"
+	} else if web.AppConfig.DefaultString("publisher_def", "") != "" {
+		defPub := web.AppConfig.DefaultString("publisher_def", "")
+		ebookConfig.Footer = "<p style='color:#8E8E8E;font-size:12px;'>本文档由 <span style='text-decoration:none;color:#1abc9c;font-weight:bold;'>" + defPub + "</span> 生成<span style='float:right'>- _PAGENUM_ -</span></p>"
 	}
 	if m.RealName != "" {
 		ebookConfig.Creator = m.RealName
